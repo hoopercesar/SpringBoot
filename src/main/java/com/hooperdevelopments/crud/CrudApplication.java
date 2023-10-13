0697924cdc7cccb965311b987acf819590e773bc
Cesar S. Hooper <86272570+hoopercesar@users.noreply.github.com>
2023-10-13 15:29:56 -0300

    Add files via upload

--- src/main/java/com/hooperdevelopments/crud/CrudApplication.java
+++ src/main/java/com/hooperdevelopments/crud/CrudApplication.java
@@ -1,31 +1,17 @@
 package com.hooperdevelopments.crud;
 
-import com.hooperdevelopments.crud.product.Product;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDate;
-import java.util.List;
 
 @SpringBootApplication
-@RestController
+
 public class CrudApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CrudApplication.class, args);
 	}
 
-	@GetMapping(path="/")
-	public List<Product> getNames() {
-		return List.of(
-				new Product(
-				2541L,
-				"Laptop V32",
-				500,
-				LocalDate.of(2010, 6, 21),
-				2
-				));
-	}
+
 }
--- src/main/java/com/hooperdevelopments/crud/product/ProductController.java
+++ src/main/java/com/hooperdevelopments/crud/product/ProductController.java
@@ -1,4 +1,25 @@
 package com.hooperdevelopments.crud.product;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@RestController
+@RequestMapping(path="api/v1/products")
 public class ProductController {
+
+    @GetMapping
+    public List<Product> getProducts() {
+        return List.of(
+                new Product(
+                        2541L,
+                        "Laptop V32",
+                        500,
+                        LocalDate.of(2010, 6, 21),
+                        2
+                ));
+    }
 }
