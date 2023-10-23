5afdf87b54cbb867aabd7785f79fa420d7ece4b6
Cesar S. Hooper <86272570+hoopercesar@users.noreply.github.com>
2023-10-22 22:52:24 -0300

    Add files via upload

---
+++ Curso/src/main/java/com/hooperdevelopments/curso/dao/UsuarioDao.java
@@ -0,0 +1,9 @@
+package com.hooperdevelopments.curso.dao;
+
+import com.hooperdevelopments.curso.models.Usuario;
+
+import java.util.List;
+
+public interface UsuarioDao {
+    List<Usuario> getUsuarios();
+}
---
+++ Curso/src/main/java/com/hooperdevelopments/curso/dao/UsuarioDaoImp.java
@@ -0,0 +1,13 @@
+package com.hooperdevelopments.curso.dao;
+
+import com.hooperdevelopments.curso.models.Usuario;
+
+import java.util.List;
+
+public class UsuarioDaoImp implements UsuarioDao{
+
+    @Override
+    public List<Usuario> getUsuarios() {
+        return null;
+    }
+}
