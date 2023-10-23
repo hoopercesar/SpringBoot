a8315b5947b48a556abef8767c18656de7114d2f
Cesar S. Hooper <86272570+hoopercesar@users.noreply.github.com>
2023-10-23 07:51:48 -0300

    Add files via upload

--- Curso/src/main/java/com/hooperdevelopments/curso/models/Usuario.java
+++ Curso/src/main/java/com/hooperdevelopments/curso/models/Usuario.java
@@ -1,59 +1,25 @@
 package com.hooperdevelopments.curso.models;
 
-public class Usuario {
-    private String nombre;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
 
+@Entity
+@Table(name="usuarios")
+public class Usuario {
+    @Getter @Setter
+    private String nombre;
+    @Getter @Setter
     private Long id;
+    @Getter @Setter
     private String apellido;
+    @Getter @Setter
     private String email;
+    @Getter @Setter
     private String telefono;
+    @Getter @Setter
     private String password;
 
-    public String getNombre() {
-        return nombre;
-    }
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getApellido() {
-        return apellido;
-    }
-
-    public void setApellido(String apellido) {
-        this.apellido = apellido;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getTelefono() {
-        return telefono;
-    }
-
-    public void setTelefono(String telefono) {
-        this.telefono = telefono;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
