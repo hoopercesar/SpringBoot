cd2ff1a33b8ae5939952a70d0b4c08ca195ee9ff
Cesar S. Hooper <86272570+hoopercesar@users.noreply.github.com>
2023-10-23 21:19:45 -0300

    Add files via upload

--- Curso/src/main/java/com/hooperdevelopments/curso/Controllers/UsuarioController.java
+++ Curso/src/main/java/com/hooperdevelopments/curso/Controllers/UsuarioController.java
@@ -1,8 +1,11 @@
 package com.hooperdevelopments.curso.Controllers;
 
+import com.hooperdevelopments.curso.dao.UsuarioDao;
 import com.hooperdevelopments.curso.models.Usuario;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -12,7 +15,10 @@ import java.util.Locale;
 @RestController
 public class UsuarioController {
 
-    @RequestMapping(value = "usuario/{id}")
+    @Autowired
+    private UsuarioDao usuarioDao;
+
+    @RequestMapping(value = "api/usuarios/{id}", method= RequestMethod.GET)
     public Usuario getUsuario(@PathVariable Long id){
         Usuario usuario = new Usuario();
         usuario.setId(id);
@@ -25,43 +31,12 @@ public class UsuarioController {
         return usuario;
     }
 
-    @RequestMapping(value = "usuarios")
+    @RequestMapping(value = "api/usuarios")
     public List<Usuario> getUsuarios(){
-        List<Usuario> usuarios = new ArrayList<>();
-
-        Usuario usuario2 = new Usuario();
-        usuario2.setId(111L);
-        usuario2.setNombre("jorge");
-        usuario2.setApellido("peres");
-        usuario2.setEmail("jorge.peres@email.com");
-        usuario2.setTelefono("9876545321");
-        usuario2.setPassword("jorgeperes123");
-
-        Usuario usuario = new Usuario();
-        usuario.setId(321L);
-        usuario.setNombre("pepe");
-        usuario.setApellido("soto");
-        usuario.setEmail("pepe@email.com");
-        usuario.setTelefono("21982996786");
-        usuario.setPassword("pepesoto123");
-
-        Usuario usuario3 = new Usuario();
-        usuario3.setId(132L);
-        usuario3.setNombre("maria");
-        usuario3.setApellido("gonzalez");
-        usuario3.setEmail("maria.gon@email.com");
-        usuario3.setTelefono("332244666789");
-        usuario3.setPassword("mariagon333");
-
-        usuarios.add(usuario);
-        usuarios.add(usuario2);
-        usuarios.add(usuario3);
-
-
-        return usuarios;
+        return usuarioDao.getUsuarios();
     }
 
-    @RequestMapping(value = "usuario23")
+    @RequestMapping(value = "api/usuario23")
     public Usuario putUsuario(){
         Usuario usuario = new Usuario();
         usuario.setNombre("pepe");
@@ -73,16 +48,8 @@ public class UsuarioController {
         return usuario;
     }
 
-
-    @RequestMapping(value = "usuario456")
-    public Usuario deleteUsuario(){
-        Usuario usuario = new Usuario();
-        usuario.setNombre("pepe");
-        usuario.setApellido("soto");
-        usuario.setEmail("pepe@email.com");
-        usuario.setTelefono("21982996786");
-        usuario.setPassword("pepesoto123");
-
-        return usuario;
+    @RequestMapping(value = "api/usuarios/{id}", method= RequestMethod.DELETE)
+    public void deleteUsuario(@PathVariable Long id){
+        usuarioDao.deleteUsuario(id);
     }
 }
