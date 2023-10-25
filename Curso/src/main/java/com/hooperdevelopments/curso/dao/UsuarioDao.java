63b557a274fb2a91fb15229f8d25519b5285ed8c
Cesar S. Hooper <86272570+hoopercesar@users.noreply.github.com>
2023-10-24 22:16:35 -0300

    Add files via upload

--- Curso/src/main/java/com/hooperdevelopments/curso/dao/UsuarioDao.java
+++ Curso/src/main/java/com/hooperdevelopments/curso/dao/UsuarioDao.java
@@ -9,4 +9,6 @@ public interface UsuarioDao {
 
 
     void deleteUsuario(Long id);
+
+    void registrar(Usuario usuario);
 }
--- Curso/src/main/java/com/hooperdevelopments/curso/dao/UsuarioDaoImp.java
+++ Curso/src/main/java/com/hooperdevelopments/curso/dao/UsuarioDaoImp.java
@@ -29,4 +29,9 @@ public class UsuarioDaoImp implements UsuarioDao{
         entityManager.remove(usuario);
 
     }
+
+    @Override
+    public void registrar(Usuario usuario) {
+        entityManager.merge(usuario);
+    }
 }
