9aca96e6d0133860c10bd5e44787c45ac8b8853d
Cesar S. Hooper <86272570+hoopercesar@users.noreply.github.com>
2023-11-05 10:57:29 -0300

    Add files via upload

--- Curso/src/main/java/com/hooperdevelopments/curso/dao/UsuarioDaoImp.java
+++ Curso/src/main/java/com/hooperdevelopments/curso/dao/UsuarioDaoImp.java
@@ -1,6 +1,8 @@
 package com.hooperdevelopments.curso.dao;
 
 import com.hooperdevelopments.curso.models.Usuario;
+import de.mkammerer.argon2.Argon2;
+import de.mkammerer.argon2.Argon2Factory;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import org.springframework.stereotype.Repository;
@@ -37,13 +39,22 @@ public class UsuarioDaoImp implements UsuarioDao{
 
     @Override
     public boolean verificarCredenciales(Usuario usuario) {
-        String query = "FROM Usuario WHERE email = :email AND password = :password";
+        String query = "FROM Usuario WHERE email = :email";
         List<Usuario> lista = entityManager.createQuery(query)
                 .setParameter("email", usuario.getEmail())
-                .setParameter("password", usuario.getPassword())
                 .getResultList();
 
-        return !lista.isEmpty();
+        if (lista.isEmpty()) {
+            return false;
+        }
+//        con esto se recupera el password en la base de datos de la lista arriba. ojo que esa lista me entrega
+//        una lista con la información del usuario que tiene un email dado. luego entonces,
+        String passwordHashed = lista.get(0).getPassword();
+
+
+        Argon2 argon2 = Argon2Factory.create(Argon2Factory.Argon2Types.ARGON2id);
+        return argon2.verify(passwordHashed, usuario.getPassword());
+
 
     }
 }
