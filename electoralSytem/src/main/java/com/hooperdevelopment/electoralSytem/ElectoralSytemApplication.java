89ea4e60dd5ca9cfd96b999c2298a76dd7717679
Cesar L. Hooper <86272570+hoopercesar@users.noreply.github.com>
2023-12-05 23:30:57 -0300

    Add files via upload

---
+++ electoralSytem/src/main/java/com/hooperdevelopment/electoralSytem/ElectoralSytemApplication.java
@@ -0,0 +1,13 @@
+package com.hooperdevelopment.electoralSytem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectoralSytemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElectoralSytemApplication.class, args);
+	}
+
+}
---
+++ electoralSytem/src/test/java/com/hooperdevelopment/electoralSytem/ElectoralSytemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hooperdevelopment.electoralSytem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectoralSytemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
