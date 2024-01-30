86f4c4e0097815e0195716ac895d101bc06d0098
Cesar L. Hooper <86272570+hoopercesar@users.noreply.github.com>
2024-01-29 23:13:20 -0300

    Add files via upload

--- SpringSecurity/Controller/CustomerController.java
+++ SpringSecurity/Controller/CustomerController.java
@@ -1,13 +1,27 @@
 package com.example.SpringSecurity.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.session.SessionInformation;
+import org.springframework.security.core.session.SessionRegistry;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 
 @RequestMapping(path="v1")
 @RestController
 public class CustomerController {
+
+    @Autowired
+    private SessionRegistry sessionRegistry;
+
+
     @GetMapping(path="/index")
     public String index() {
         return "Hola, mundo";
@@ -15,5 +29,34 @@ public class CustomerController {
     @GetMapping(path="/index2")
     public String index2() {
         return "Non Secure Hola Mundo";
+    }
+
+    @GetMapping("/session")
+    public ResponseEntity<?> getDetailSessions() {
+
+        String sessionId = "";
+        User userObject = null;
+
+        List<Object> sessions = sessionRegistry.getAllPrincipals();
+
+        for (Object session : sessions) {
+            if (session instanceof User) {
+                userObject = (User) session;
+            }
+
+            List<SessionInformation> sessionInformations = sessionRegistry.getAllSessions(session, false);
+
+            for (SessionInformation sessionInformation : sessionInformations){
+                sessionId = sessionInformation.getSessionId();
+            }
+        }
+
+        Map<String, Object> response = new HashMap<>();
+
+        response.put("response", "hello world");
+        response.put("sessionId", sessionId);
+        response.put("userObject", userObject);
+
+        return ResponseEntity.ok(response);
     }
 }
