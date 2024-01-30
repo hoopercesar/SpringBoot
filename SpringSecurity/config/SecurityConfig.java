edf5cae3d000b03d0289b0d68d0569c623bf3df6
Cesar L. Hooper <86272570+hoopercesar@users.noreply.github.com>
2024-01-29 23:13:53 -0300

    Add files via upload

--- SpringSecurity/config/SecurityConfig.java
+++ SpringSecurity/config/SecurityConfig.java
@@ -4,7 +4,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.session.SessionRegistry;
+import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 @Configuration
 @EnableWebSecurity
@@ -25,14 +29,37 @@ public class SecurityConfig {
 //    configuration 2
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        //System.out.println(httpSecurity.build());
         return httpSecurity
                 .authorizeHttpRequests( auth -> {
                     auth.requestMatchers("v1/index2").permitAll();
-                            auth.anyRequest().authenticated();
+                    auth.anyRequest().authenticated();
                 })
                 .formLogin().permitAll()
+                    .successHandler(successHandler()) // URL donde se dirige después de iniciar session
+                    .permitAll()
+                .and()
+                .sessionManagement() // gestión de session de users
+                    .sessionCreationPolicy(SessionCreationPolicy.ALWAYS) // ALWAYS - IF_REQUIRED - NEVER - STATELESS
+                    .invalidSessionUrl("/login")
+                    .maximumSessions(1)
+                    .expiredUrl("/login")
+                    .sessionRegistry(sessionRegistry())
+                .and()
+                .sessionFixation()
+                    .migrateSession() // migrateSession - newSession - noneSession
                 .and()
                 .build();
+    }
+
+    public AuthenticationSuccessHandler successHandler() {
+        return ((request, response, authentication) -> {
+            response.sendRedirect("v1/session");
+        });
+    }
+    @Bean
+    public SessionRegistry sessionRegistry() {
+        return new SessionRegistryImpl();
     }
 
 }
