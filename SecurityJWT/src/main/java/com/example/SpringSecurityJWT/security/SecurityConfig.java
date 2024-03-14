d7f7ea5afd6571c0fad0c3795d75a55da697329b
Cesar L. Hooper <86272570+hoopercesar@users.noreply.github.com>
2024-03-13 22:45:18 -0300

    Add files via upload

--- SecurityJWT/src/main/java/com/example/SpringSecurityJWT/security/SecurityConfig.java
+++ SecurityJWT/src/main/java/com/example/SpringSecurityJWT/security/SecurityConfig.java
@@ -2,12 +2,22 @@ package com.example.SpringSecurityJWT.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+
+    // manejo de acceso de los endpoints con autenticación básica
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
 
@@ -24,6 +34,31 @@ public class SecurityConfig {
                 .and()
                 .build();
 
+    }
+    // la autenticación básica se hace con un usuario
+    @Bean
+    UserDetailsService userDetailsService() {
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+
+        manager.createUser(User.withUsername("cesar")
+                .password("12334")
+                .roles()
+                .build());
+
+        return manager;
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    AuthenticationManager authenticationManager(HttpSecurity httpSecurity, PasswordEncoder passwordEncoder) throws Exception {
+        return httpSecurity.getSharedObject(AuthenticationManagerBuilder.class)
+                .userDetailsService(userDetailsService())
+                .passwordEncoder(passwordEncoder)
+                .and().build();
     }
 }
 
