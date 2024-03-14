857a2edd91ed2e7ec0beaeb4511e340420968e57
Cesar L. Hooper <86272570+hoopercesar@users.noreply.github.com>
2024-03-14 20:09:54 -0300

    Add files via upload

---
+++ SecurityJWT/src/main/java/com/example/SpringSecurityJWT/security/filters/JwtAuthenticationFilter.java
@@ -0,0 +1,62 @@
+package com.example.SpringSecurityJWT.security.filters;
+
+import com.example.SpringSecurityJWT.models.UserEntity;
+import com.example.SpringSecurityJWT.security.jwt.JwtUtils;
+import com.fasterxml.jackson.core.exc.StreamReadException;
+import com.fasterxml.jackson.databind.DatabindException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.io.IOException;
+
+public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+    private JwtUtils jwtUtils;
+
+    public JwtAuthenticationFilter(JwtUtils jwtUtils) {
+        this.jwtUtils = jwtUtils;
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request,
+                                                HttpServletResponse response) throws AuthenticationException {
+        UserEntity userEntity = null;
+        String username = "";
+        String password = "";
+        try {
+            userEntity = new ObjectMapper().readValue(request.getInputStream(), UserEntity.class);
+            username = userEntity.getUsername();
+            password = userEntity.getPassword();
+        } catch (StreamReadException e) {
+            throw new RuntimeException(e);
+        } catch (DatabindException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        UsernamePasswordAuthenticationToken authenticationToken =
+                new UsernamePasswordAuthenticationToken(username, password);
+
+        return getAuthenticationManager().authenticate(authenticationToken);
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request,
+                                            HttpServletResponse response,
+                                            FilterChain chain,
+                                            Authentication authResult) throws IOException, ServletException {
+        User user = (User) authResult.getPrincipal();
+        String token = jwtUtils.generateAccesToken(user.getUsername());
+        super.successfulAuthentication(request, response, chain, authResult);
+    }
+}
