20370a4d364b6c0abe056e77b1c78065111071e3
Cesar L. Hooper <86272570+hoopercesar@users.noreply.github.com>
2024-03-17 18:48:36 -0300

    Add files via upload

--- SecurityJWT/src/main/java/com/example/SpringSecurityJWT/security/filters/JwtAuthenticationFilter.java
+++ SecurityJWT/src/main/java/com/example/SpringSecurityJWT/security/filters/JwtAuthenticationFilter.java
@@ -10,6 +10,8 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
@@ -17,6 +19,8 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 
@@ -57,6 +61,20 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
                                             Authentication authResult) throws IOException, ServletException {
         User user = (User) authResult.getPrincipal();
         String token = jwtUtils.generateAccesToken(user.getUsername());
+
+        response.addHeader("Authorization", token);
+
+        Map<String, Object> httpResponse = new HashMap<>();
+        httpResponse.put("token", token);
+        httpResponse.put("Message", "Autenticación Correcta");
+        httpResponse.put("Username", user.getUsername());
+
+        response.getWriter().write(new ObjectMapper().writeValueAsString(httpResponse));
+        response.setStatus(HttpStatus.OK.value());
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.getWriter().flush();
+
+
         super.successfulAuthentication(request, response, chain, authResult);
     }
 }
